0ce51c255d2b2a31ce84f041ef9280a03f69a46b
alex casadio <alex.casadio92@gmail.com>
2021-06-05 21:18:56 +0200

    Added UnitOfWork implementation.

---
+++ Infrastructure.Contracts/Repositories/Foundation/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Infrastructure.Contracts.Repositories.Foundation
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAccountRepository Accounts { get; }
+        IProfileRepository Profiles { get; }
+        int Complete();
+    }
+}
---
+++ Infrastructure.Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Contracts.Repositories;
+using Infrastructure.Contracts.Repositories.Foundation;
+using Infrastructure.Entities.Contexts;
+
+namespace Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SPPMContext _context;
+
+        public UnitOfWork(SPPMContext context)
+        {
+            _context = context;
+            Accounts = new AccountRepository(_context);
+            Profiles = new ProfileRepository(_context);
+        }
+
+        public IAccountRepository Accounts { get; }
+        public IProfileRepository Profiles { get; }
+        
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+        
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+    }
+}
