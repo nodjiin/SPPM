a0c8bfd4e3133823f186169a8eeece95e97805fc
alex casadio <alex.casadio92@gmail.com>
2021-06-05 22:10:00 +0200

    Small update on foundation interfaces

--- DomainModel.Contracts/Foundation/IHasPassword.cs
+++ DomainModel.Contracts/Foundation/IHasPassword.cs
@@ -2,6 +2,6 @@
 {
     public interface IHasPassword
     {
-        public string Password { get; set; }
+        string Password { get; set; }
     }
 }
--- DomainModel.Contracts/Foundation/IHasUsername.cs
+++ DomainModel.Contracts/Foundation/IHasUsername.cs
@@ -2,6 +2,6 @@
 {
     public interface IHasUsername
     {
-        public string Username { get; set; }
+        string Username { get; set; }
     }
 }
