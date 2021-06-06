1225304df132674b2769faa0d1de918bca988b57
alex casadio <alex.casadio92@gmail.com>
2021-06-06 14:35:22 +0200

    Moved connection string to configuration file.

--- Infrastructure.Entities/Contexts/SPPMContext.cs
+++ Infrastructure.Entities/Contexts/SPPMContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Model;
+﻿using System.Configuration;
+using Domain.Model;
 using Microsoft.EntityFrameworkCore;
 
 #nullable disable
@@ -23,8 +24,7 @@ namespace Infrastructure.Entities.Contexts
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=SPPM;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["SPPMContext"].ConnectionString);
             }
         }
 
